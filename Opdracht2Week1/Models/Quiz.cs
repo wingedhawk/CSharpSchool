f2beff76b1fbc8421038d3d7418c7d135ccdd69b
Jeffrey Lo-A-Foe <loafoe.jeffrey@gmail.com>
2018-11-16 11:59:54 +0100

    Added Quiz.model and Vraag.interface

---
+++ Opdracht2Week1/Models/Quiz.cs
@@ -0,0 +1,21 @@
+﻿using Opdracht2Week1.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Opdracht2Week1
+{
+    class Quiz
+    {
+        public List<Vraag> vragen = new List<Vraag>();
+
+        public List<Vraag> getOrderedLijst()
+        {
+            var returnVragen = vragen.OrderBy(v => v.CategorieString).ToList();
+            return returnVragen;
+        }
+
+    }
+
+}
---
+++ Opdracht2Week1/Models/Vraag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht2Week1.Models
+{
+    interface Vraag
+    {
+        int Moeilijkheidsgraad { get; set; }
+        string CategorieString { get; set; }
+        string Antwoord { get; set; }
+    }
+
+}
