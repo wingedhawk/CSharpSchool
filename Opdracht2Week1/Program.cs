3f9cb305ca68c2a96fc88e5b9648d99369b3abc9
Jeffrey Lo-A-Foe <loafoe.jeffrey@gmail.com>
2018-11-16 11:36:00 +0100

    Init

---
+++ Opdracht2Week1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opdracht2Week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
